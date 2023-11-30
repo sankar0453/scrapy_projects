806bbf12f22e52ec51efa07a0763892a61643392
sankar0453 <sankar0453@gmail.com>
2023-11-30 22:32:11 +0530

    api with pagination

--- api_project/api_project/spiders/quotes.py
+++ api_project/api_project/spiders/quotes.py
@@ -16,3 +16,14 @@ class QuotesSpider(scrapy.Spider):
                 "tag":quote.get('tags') ,
                 "quotes":quote.get('text') 
             }
+        
+        # Picking the "has_next" element
+        has_next = json_response.get('has_next')
+
+        # If has_next==True (there's next page), execute the following code
+        if has_next:
+            next_page_number = json_response.get('page')+1
+            yield scrapy.Request(
+                url=f'https://quotes.toscrape.com/api/quotes?page={next_page_number}',
+                callback=self.parse
+            )
