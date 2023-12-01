9cc942e4d796e5cd81a48c9867a3f27ed170bdfb
sankar0453 <sankar0453@gmail.com>
2023-12-01 09:36:30 +0530

    parsed eu number and URL

--- euctr_project/euctr_project/spiders/euro_trials.py
+++ euctr_project/euctr_project/spiders/euro_trials.py
@@ -4,7 +4,21 @@ import scrapy
 class EuroTrialsSpider(scrapy.Spider):
     name = "euro_trials"
     allowed_domains = ["www.clinicaltrialsregister.eu"]
-    start_urls = ["https://www.clinicaltrialsregister.eu/ctr-search/search?query="]
+    start_urls = ["https://www.clinicaltrialsregister.eu/ctr-search/search?query=&page=4"]
 
     def parse(self, response):
-        pass
+        table_container = response.xpath('//div[contains(@class , "result")]/table')
+        for table in table_container:
+            eudract_number = table.xpath('.//span[contains(text(), "EudraCT Number")]/following-sibling::text()').get()
+            protocol_elements = table.xpath('.//span[contains(text(), "Trial protocol")]/following-sibling::a')
+            
+            for protocol_element in protocol_elements:
+                protocol_url = protocol_element.xpath('@href').get()
+                
+                if eudract_number  :
+                    yield {
+                        'EudraCT Number': eudract_number.strip(),
+                        'Trial Protocol': protocol_url.strip(),
+                    } 
+
+ 
